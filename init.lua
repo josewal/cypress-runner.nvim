ec201bf1c8e3dcb2fb2fa5e2e3bb12beccff1f0c
josefwalek <josef.walek@gmail.com>
2024-01-24 14:23:45 +0100

    .cy.ts check

--- init.lua
+++ init.lua
@@ -2,9 +2,14 @@ local M = {}
 
 M.run_cypress_spec = function()
 	local current_file = vim.fn.expand("%:p") -- Get the full path of the current file
-	local command = "npx cypress run --headless --browser chrome --spec " .. current_file
 
-	vim.cmd("! " .. command) -- Execute the command in a shell
+	-- Check if the current file ends with .cy.ts
+	if string.match(current_file, "%.cy%.ts$") then
+		local command = "npx cypress run --headless --spec " .. current_file
+		vim.cmd("! " .. command) -- Execute the command in a shell
+	else
+		print("Not a .cy.ts file")
+	end
 end
 
 return M
