1bd3e1678223e5d11fc3ca5184a7763c44e17fcb
josefwalek <josef.walek@gmail.com>
2024-01-24 15:29:24 +0100

    space escaping

--- lua/cypress-runner/init.lua
+++ lua/cypress-runner/init.lua
@@ -3,6 +3,7 @@ local M = {}
 -- Function to run the Cypress command
 M.run_cypress_spec = function()
 	local current_file = vim.fn.expand("%:p")
+	current_file = string.gsub(current_file, " ", "\\ ")
 	local command = "npx cypress run --headless --spec " .. current_file
 
 	-- Open a new window at the bottom and switch to it
