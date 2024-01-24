ff4e3a6a817425e41e46fb6a4b208c7568f7e4f3
josefwalek <josef.walek@gmail.com>
2024-01-24 14:44:52 +0100

    window

--- lua/cypress-runner/init.lua
+++ lua/cypress-runner/init.lua
@@ -2,14 +2,19 @@ local M = {}
 
 M.run_cypress_spec = function()
 	local current_file = vim.fn.expand("%:p") -- Get the full path of the current file
+	local command = "npx cypress run --headless --spec " .. current_file
 
-	-- Check if the current file ends with .cy.ts
-	if string.match(current_file, "%.cy%.ts$") then
-		local command = "npx cypress run --headless --spec " .. current_file
-		vim.cmd("! " .. command) -- Execute the command in a shell
-	else
-		print("Not a .cy.ts file")
-	end
+	-- Open a new split at the bottom
+	vim.cmd("botright new")
+
+	-- Make the new window a little bigger
+	vim.cmd("resize 10")
+
+	-- Run the command in the terminal in the new window
+	vim.cmd("term " .. command)
+
+	-- Go back to the previous (original) window
+	vim.cmd("wincmd p")
 end
 
 return M
